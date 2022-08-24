d868258c0b4ab434b9e2e1489a55c2ce816138b9
tedtalktimmy <frtntn007@gmail.com>
2022-08-23 21:12:52 -0400

    adjusted html page and added styling via css

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+        // start button gets click function
+
+// THEN a timer starts and I am presented with a question
+        // once start button gets clicked, the timer starts and a question comes up
+
+// WHEN I answer a question
+        // Answer the question, click the button
+
+// THEN I am presented with another question
+        // upon clicking the button next, next question comes up
+
+// WHEN I answer a question incorrectly
+        // if function for question answered incorectly.
+
+// THEN time is subtracted from the clock
+        // if question is answered incorrectly, time is subtracted from clock
+
+// WHEN all questions are answered or the timer reaches 0
+        // if function for all question answered or time runs out. 
+
+// THEN the game is over
+        // if either of the above happens, the game ends
+
+// WHEN the game is over
+        // game ends
+
+// THEN I can save my initials and my score
+        // ability to save initials and score
